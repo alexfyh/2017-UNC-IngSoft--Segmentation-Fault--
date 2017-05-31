e477a751153f0a8e6ee3ed8e494bab15cf291131
alexfyh <alexander.fyh@gmail.com>
2017-05-31 01:43:09 -0300

    reempalzando enies y reformat

--- src/main/java/Bibliotecario.java
+++ src/main/java/Bibliotecario.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 /**
  * Created by Fabrizio_p on 27/05/2017.
  */
-public class Bibliotecario extends Afiliado{
-    private String contraseña;
+public class Bibliotecario extends Afiliado {
+    private String contrasena;
     public static int minContr = 6;
     public static int maxContr = 15;
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
 
-    public Bibliotecario(String id, String nombre, String apellido, String tel, String direccion, String laContraseña) throws Exception {
+    public Bibliotecario(String id, String nombre, String apellido, String tel, String direccion, String laContrasena) throws Exception {
         super(id, nombre, apellido, tel, direccion);
 
-        setContraseña(laContraseña);
+        setContrasena(laContrasena);
 
     }
 
 
-    public void setContraseña(String contras){
-        while (contras.length()<= minContr || contras.length() > maxContr){
-            System.out.print("No se ha podido crear contraseña, reingrese") ;
+    public void setContrasena(String contras) {
+        while (contras.length() <= minContr || contras.length() > maxContr) {
+            System.out.print("No se ha podido crear contraseña, reingrese");
             System.out.print("min 6 caracteres, max 15: ");
-            this.contraseña = scanner.nextLine();
+            this.contrasena = scanner.nextLine();
         }
         System.out.println("La contraseña se cambió exitosamente");
     }
