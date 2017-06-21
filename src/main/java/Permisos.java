ef98ceb6fb470bac97b98c32d8453c1744c2de75
FabrizioPerez <fabrizioperez222@gmail.com>
2017-06-21 15:40:55 -0300

    Agregando permisos

--- src/main/java/Permisos.java
+++ src/main/java/Permisos.java
@@ -2,9 +2,31 @@
  * Created by YepezHinostroza on 17/6/2017.
  */
 public interface Permisos {
+
+    public void prestar(Afiliado afiliado, Ejemplar ejemplar);
+    public void devolver(Ejemplar ejemplar); // ver si hace hace falta pasarle el afiliado
+
+    public void setFecha(BibliotecaModel modelo);
+
+    public void afiliar(Afiliado afiliado,RegistroAfiliados registro);
+    public void desafiliar(Integer dni,RegistroAfiliados registro);
+    public void modPermisos(Integer dni,RegistroAfiliados registro);
+
+    public void agregarEjemplar(Ejemplar ejemplar,RegistroItems items);
+    public void eliminarEjemplar(Integer IdEjemplar, RegistroItems items );
+
+    public void consultarDisponibilida(Integer idItem,RegistroItems items);
+    public void verInfo(Integer idItem);
+    public void filtrarPorCategoria();
+
+
+
+
+    public void logout();
+    public void login(Integer dni, String password);
+
     // prestamo
     //login
     //logout
-    //consultas
-    //popu popu po putona
+
 }
